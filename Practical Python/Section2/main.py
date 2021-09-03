45dbd348808ad48f282c486e1fc5b31033707ada
Ryne <rhyno466@gmail.com>
2021-09-02 21:00:08 -0400

    through 2.4

---
+++ Practical Python/Section2/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  2 20:20:49 2021
+
+@author: rhyno466
+"""
+# Ask user name
+user_name = input("What is your name: ")
+
+# Ask user age
+user_age = input("WHhat is your age: ")
+
+# Print input
+print("\nHello " + user_name + " your are " + user_age + " years old!")
+
+
+# This is a comment

6e618ff02544f5c715a86e0976b49ca712e122ea
Ryne <rhyno466@gmail.com>
2021-09-22 21:40:14 -0400

    Through 3.3

---
+++ Practical Python/Section3/level3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 22 20:46:40 2021
+
+@author: rhyno46655
+"""
+
+user_age = 25
+month = "July"
+
+if user_age >= 18 and user_age < 30:
+    print("You are an adult below 30")
+
+if month == "July":
+    print("Summer Time")
+
+print("End of Porgram")

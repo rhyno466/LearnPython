e8fcc41af2c352f5d7a420ae5785a4a46e2a7a37
Ryne <rhyno466@gmail.com>
2021-09-22 21:18:00 -0400

    Section 2 Complete
    After Solution

--- Practical Python/Section2/2p9_exercises.py
+++ Practical Python/Section2/2p9_exercises.py
@@ -6,16 +6,18 @@ Created on Sun Sep 12 13:54:12 2021
 """
 #%% Functions
 
-def get_user_info():
-    user_name = input("Enter user name: \n")
-    user_age = input("Enter user age: \n")
-    print("\nYou are " + user_name + ", who is " + user_age + " years old!")
+# def get_user_info():
+#     user_name = input("Enter user name: \n")
+#     user_age = input("Enter user age: \n")
+#     print("\nYou are " + user_name + ", who is " + user_age + " years old!")
 
 
-def addition():
-    num_1 = float(input("Enter a number: \n"))
-    num_2 = float(input("Enter another number: \n"))
-    total = int(num_1 + num_2)
+def get_user_info(user_name, user_age):
+    print("\nYou are " + user_name + ", who is " + str(user_age) + " years old!")
+
+
+def addition(a, b):
+    total = int(a + b)
     print("The sum of the numbers is : " + str(total))
 
 
@@ -32,19 +34,24 @@ def compute_average():
 def compute_average_list(lst):
     avg = sum(lst) / len(lst)
     print("The average of the numbers is " + str(avg))
+    return avg
 
 
 def degC_to_degF(c):
     f = c * 1.8 + 32
-    print(str(c) + 'deg C is equal to ' + str(f) + 'deg F')
+    print(str(c) + "deg C is equal to " + str(f) + "deg F")
+    return f
+
 
 #%% Other
+n = "Ryne"
+a = 33
+get_user_info(n, a)
+
+num_1 = float(input("Enter a number: \n"))
+num_2 = float(input("Enter another number: \n"))
+addition(num_1, num_2)
 
 numbers = [1, 2, 44.5, 12]
 average = sum(numbers) / len(numbers)
-print('The average of the numbers is ' + str(average))
-
-
-
-
-
+print("The average of the numbers is " + str(average))

1bbe3ff230ef5db0454690cbe467e276c7f92bf1
Ryne <rhyno466@gmail.com>
2021-09-23 20:48:51 -0400

    Through 3.5

--- Practical Python/Section3/level3.py
+++ Practical Python/Section3/level3.py
@@ -6,13 +6,22 @@ Created on Wed Sep 22 20:46:40 2021
 @author: rhyno46655
 """
 
-user_age = 25
-month = "July"
+user_age = 176
+month = "May"
 
-if user_age >= 18 and user_age < 30:
-    print("You are an adult below 30")
+if user_age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult yet")
 
-if month == "July":
+if month in ["June", "July", "August"]:
     print("Summer Time")
+elif month in ["September", "October", "November"]:
+    print("Autumn Time")
+elif month in ["December", "January", "February"]:
+    print("Winter Time")
+else:
+    print("Spring Time")
+
 
 print("End of Porgram")

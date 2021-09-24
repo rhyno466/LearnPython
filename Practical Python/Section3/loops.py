dabb810a70d421efcc8d3491595233104636bde6
Ryne <rhyno466@gmail.com>
2021-09-23 20:58:21 -0400

    Through 3.7

---
+++ Practical Python/Section3/loops.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 20:49:26 2021
+
+@author: rhyno466
+"""
+#%% poverty way
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+print("hello")
+
+#%% For Loop
+for i in range(100):
+    print("Hello " + str(i))
+# print('test')
+
+#%% While Loop
+i = 0
+while i < 10:
+    print("Hello " + str(i))
+    i += 1

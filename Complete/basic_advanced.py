67cc2e504447384a7d94def0e4c7a213e28cdf4f
Ryne <rhyno466@gmail.com>
2021-09-28 21:42:03 -0400

    through vars

---
+++ Complete/basic_advanced.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 28 21:06:43 2021
+
+@author: rhyno466
+"""
+#%% line strut
+x = 1
+y = 1
+
+z = 1
+
+# this is a comment
+
+b = 1 + 2
+c = 1 + 2
+
+r = 1
+v = 3
+oo = 9.0
+#%% comments
+summing = 3 + 4  # this lines sums 2 nums
+
+# this is a comment line
+
+"""
+lots if comments
+
+a shit tonne
+"""
+
+#%% joining lines
+x = 4
+y = 3
+
+z = x + y
+
+#%% mult statement single line
+x = 3
+y = 4
+z = x + y
+
+
+#%% indentation
+
+#%% variables
+x = 4 + 5
+print(x)
+result_two = 4 + 4
+

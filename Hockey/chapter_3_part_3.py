edaf7b3ac7a6f1baac1974ed15dd57fb0c796208
Ryne <rhyno466@gmail.com>
2023-02-21 17:30:22 -0500

    hockey ch3p2

--- Hockey/chapter_3_part_3.py
+++ Hockey/chapter_3_part_3.py
@@ -17,3 +17,25 @@ pg[['shots', 'goals', 'assists','hits']].mean(axis=0)
 pg[['shots', 'goals', 'assists','hits']].mean(axis=1).head()
 
 # %%
+pg['defender_score'] = (pg['pos'] == 'D') & (pg['goals'] > 0)
+pg[['defender_score', 'name', 'goals']].sample(5)
+pg['defender_score'].mean()
+# %%
+pg['defender_score'].sum()
+# %%
+(pg['pen_min'] > 20).any()
+# %%
+(pg['time_ice'] > 0).all()
+# %%
+(pg[['goals', 'assists']] > 2).any(axis=1)
+# %%
+(pg[['goals', 'assists']] > 2).any(axis=1).sum()
+# %%
+(pg[['time_ice_sh', 'time_ice_pp']] == 0).all(axis=1).sum()
+# %%
+pg['pos'].value_counts()
+# %%
+pg['pos'].value_counts(normalize=True,)
+# %%
+pd.crosstab(pg['team'], pg['pos']).sample(5)
+# %%
